ed1453cab18fdbdceea0568c80d952f789dd6f2d
Michael Osarodion <macmive@gmail.com>
2020-08-18 15:59:11 +0100

    add_marks created

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def create_student_dictionary():
-
     name = input("Enter student name: ")
     student_record = {'name': name,
                       'marks': []
@@ -8,5 +7,20 @@ def create_student_dictionary():
     return student_record
 
 
+# student_mark: mark appended to the list in the student dictionary
+def add_marks(student, mark):
+    student['marks'].append(mark)
+
+    # return None
+
+
 if __name__ == '__main__':
-    print(create_student_dictionary())
+    s = create_student_dictionary()
+    add_marks(s, 40)  # passing by reference
+    print(s)
+
+'''
+A Python variable points to a memory location & the value of variable is stored in that location
+Passing by reference: when a function changes the 
+original value of the variable using a reference. 
+'''
