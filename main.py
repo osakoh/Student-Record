3dc6021929d6ae00fa8558df90a9ec2809a15fad
Michael Osarodion <macmive@gmail.com>
2020-08-19 11:46:42 +0100

    coverted student dictionary to class & created main_options function

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 student_list = []
 
 
-def create_student_dictionary():
-    name = input("Enter student name: ")
-    student_record = {'name': name,
-                      'marks': []
-                      }
+class Student:
+    # constructor or initializer
+    def __init__(self):
+        self.name = "Pau"
+        self.marks = []
 
-    return student_record
+
+def create_student():
+    name = input("Enter student name: ")
+    student_data = Student()
+
+    return student_data
 
 
 # Argument mark is the mark appended to the list in the student dictionary
 def add_marks(student, mark):
-    student['marks'].append(mark)
+    student.marks.append(mark)
 
     # return None
 
@@ -25,17 +30,17 @@ def my_sum(my_list):
 
 
 def calculate_average(student):
-    if len(student['marks']) == 0:
+    if len(student.marks) == 0:
         return 0
 
-    # total_marks = sum(student['marks'])
-    total_marks = my_sum(student['marks'])
-    average = total_marks/len(student['marks'])
+    # total_marks = sum(student.marks)
+    total_marks = my_sum(student.marks)
+    average = total_marks/len(student.marks)
     return average
 
 
 def individual_student_record(student):
-    print("Name: {}\nMarks: {}\nAverage: {}\n".format(student['name'], student['marks'], calculate_average(student)))
+    print("Name: {}\nMarks: {}\nAverage: {}\n".format(student.name, student.marks, calculate_average(student)))
 
 
 def all_student_record(temp_list):
@@ -44,32 +49,37 @@ def all_student_record(temp_list):
         individual_student_record(student)
 
 
-def menu():
+def menu_options():
     print("_______________________________________\n"
           "¦____ Student Registration System ____¦\n"
-          "| 1.        Print student list        |\n"
-          "| 2.        Add student to list       |\n"
-          "| 3.        Add mark to student       |\n"
+          "| 1.        Create student name       |\n"
+          "| 2.        Add mark to student       |\n"
+          "| 3.        Print student list        |\n"
           "| 0.        Exit application          |\n"
           "|_____________________________________|")
+
+
+def menu():
+    menu_options()
     selection = int(input("Enter selection: "))
 
     while selection != 0:
 
-        if selection == 1:
+        if selection == 3:
             if len(student_list) < 1:
                 print("No details yet, Please create a student first!\n")
             all_student_record(student_list)
+        elif selection == 1:
+            student = create_student()
+            student_list.append(student)
+            print("{} created".format(student.name))
+            menu_options()
         elif selection == 2:
-            # student = create_student_dictionary()
-            # student_list.append(student)
-            student_list.append(create_student_dictionary())
-        elif selection == 3:
             if len(student_list) < 1:
                 print("Can't perform action!\n")
             else:
                 for index, student in enumerate(student_list):
-                    print("ID.{} -> {}".format(index, student['name']))
+                    print("ID.{} -> {}".format(index, student.name))
                 student_id = int(input("Select student ID: "))
 
                 student = student_list[student_id]
@@ -77,13 +87,14 @@ def menu():
                 mark = int(input("Input student mark: "))
 
                 add_marks(student, mark)
+                menu_options()
         print()
         selection = int(input("Enter selection: "))
 
 
 if __name__ == '__main__':
     menu()
-    # s = create_student_dictionary()
+    # s = create_student()
     # print(calculate_average(s))
     # add_marks(s, 150)  # passing by reference
     # print(calculate_average(s))
@@ -92,7 +103,7 @@ if __name__ == '__main__':
     # individual_student_record(s)
     # print()
     #
-    # a = create_student_dictionary()
+    # a = create_student()
     # print(calculate_average(a))
     # add_marks(a, 50)  # passing by reference
     # print(calculate_average(s))
@@ -101,7 +112,7 @@ if __name__ == '__main__':
     # individual_student_record(a)
     # print()
     #
-    # b = create_student_dictionary()
+    # b = create_student()
     # print(calculate_average(b))
     # add_marks(b, 100)  # passing by reference
     # print(calculate_average(s))
